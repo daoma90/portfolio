c6e1e314d18ac30d26b1041dc6c486907a9e89ae
David <Daoma90@gmail.com>
2023-05-27 13:18:02 +0200

    Change page transition

--- components/library/atoms/PageTransition/index.tsx
+++ components/library/atoms/PageTransition/index.tsx
@@ -14,16 +14,36 @@ const PageTransition: FC<PageTransitionProps> = ({}) => {
         initial={{ x: "0%" }}
         animate={{ x: "100%" }}
         transition={{
+          delay: 0.3,
           duration: utils.pageTransitionDuration,
-          ease: [0.7, 0, 0, 1],
+          ease: [0, 0.89, 0.41, 1],
         }}
       />
+      <PageEnter2
+        initial={{ x: "0%" }}
+        animate={{ x: "100%" }}
+        transition={{
+          duration: utils.pageTransitionDuration,
+          ease: [0, 0.89, 0.41, 1],
+        }}
+      />
+
       <PageExit
         initial={{ x: "-100%" }}
         animate={{ x: "-100%" }}
         exit={{ x: "0%" }}
-        transition={{ duration: utils.pageTransitionDuration, ease: [0.7, 0, 0, 1] }}
+        transition={{ duration: utils.pageTransitionDuration, ease: [0, 0.89, 0.41, 1] }}
         key="page-exit"
+      />
+      <PageExit2
+        initial={{ x: "-100%" }}
+        animate={{ x: "-100%" }}
+        exit={{ x: "0%" }}
+        transition={{
+          duration: utils.pageTransitionDuration,
+          delay: 0.3,
+          ease: [0, 0.89, 0.41, 1],
+        }}
       />
     </>
   );
@@ -32,21 +52,41 @@ const PageTransition: FC<PageTransitionProps> = ({}) => {
 export default PageTransition;
 
 const PageEnter = styled(m.div)`
-  position: absolute;
+  position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  z-index: 999;
+  z-index: 100;
   background-color: ${(props) => props.theme.colors.pageTransition};
 `;
 
-const PageExit = styled(m.div)`
-  position: absolute;
+const PageEnter2 = styled(m.div)`
+  position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  z-index: 999;
+  z-index: 100;
+  background-color: ${(props) => props.theme.colors.purple};
+`;
+
+const PageExit = styled(m.div)`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
   background-color: ${(props) => props.theme.colors.pageTransition};
+`;
+
+const PageExit2 = styled(m.div)`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: ${(props) => props.theme.colors.purple};
+  z-index: 100;
 `;
