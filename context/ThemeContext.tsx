45a865a4ee3a7163044733aa6499cc2d15c91bca
David <Daoma90@gmail.com>
2023-05-27 16:58:14 +0200

    remove unneeded code

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -25,26 +25,26 @@ export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
   const [theme, setTheme] = useState<string>("dark");
 
   const setDarkTheme = () => {
-    handleSetTransition();
+    // handleSetTransition();
     setTheme("dark");
   };
 
   const setLightTheme = () => {
-    handleSetTransition();
+    // handleSetTransition();
     setTheme("light");
   };
 
-  const handleSetTransition = () => {
-    if (document) {
-      const body = document.querySelector("body");
-      if (body) {
-        body.style.transition = `background-color ${utils.themeColorTransitionDuration}s linear, color ${utils.themeColorTransitionDuration}s linear`;
-        setTimeout(() => {
-          body.style.transition = "unset";
-        }, 1000);
-      }
-    }
-  };
+  // const handleSetTransition = () => {
+  //   if (document) {
+  //     const body = document.querySelector("body");
+  //     if (body) {
+  //       body.style.transition = `background-color ${utils.themeColorTransitionDuration}s linear, color ${utils.themeColorTransitionDuration}s linear`;
+  //       setTimeout(() => {
+  //         body.style.transition = "unset";
+  //       }, 1000);
+  //     }
+  //   }
+  // };
 
   const value = {
     theme,
