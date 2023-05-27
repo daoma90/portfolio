25ff3cdad0a1c7f83eab5b776b198648c9f8f739
David <Daoma90@gmail.com>
2023-05-27 13:16:58 +0200

    make cursor component dynamic import

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -5,9 +5,10 @@ import Container from "./Container";
 import { useThemeContext } from "@/context/ThemeContext";
 import CustomCursor from "../library/atoms/CustomCursor";
 import dynamic from "next/dynamic";
-import Splash from "./Splash";
+import Splash from "../library/atoms/Splash";
 
-const DynamicFooter = dynamic(() => import("./Footer"), { ssr: false });
+const DynamicFooter = dynamic(() => import("./Footer/glow"), { ssr: false });
+const DynamicCursor = dynamic(() => import("../library/atoms/CustomCursor"), { ssr: false });
 
 interface LayoutProps {
   children: ReactNode;
@@ -24,7 +25,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
         <DynamicFooter />
       </Container>
       {/* <Splash /> */}
-      <CustomCursor />
+      <DynamicCursor />
     </Theme>
   );
 };
