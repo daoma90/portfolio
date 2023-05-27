13c7fdfef404e8dc6678598de8620c55d112264c
David <Daoma90@gmail.com>
2023-05-27 13:17:10 +0200

    refactor footer

---
+++ components/layout/Footer/glow/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as s from "./styles";
+
+const Footer = () => {
+  return (
+    <s.Container>
+      <s.Glow />
+      <s.Border />
+      <s.Background />
+    </s.Container>
+  );
+};
+
+export default Footer;
