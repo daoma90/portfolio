40b8e0f24098ccf65d2078543566e2c9f5893650
David <Daoma90@gmail.com>
2023-05-28 17:00:33 +0200

    Add enter and exit animations

--- components/layout/Footer/glow/index.tsx
+++ components/layout/Footer/glow/index.tsx
@@ -1,55 +1,74 @@
 import CustomLink from "@/components/library/atoms/CustomLink";
 import Icons from "@/components/library/atoms/Icons";
 import { useMouseContext } from "@/context/MouseContext";
-import React from "react";
+import React, { FC } from "react";
 import * as s from "./styles";
 
-const Footer = () => {
+interface FooterProps {
+  direction: string;
+  index: number;
+  origin: number;
+}
+
+const Footer: FC<FooterProps> = ({ direction, index, origin }) => {
   const { cursorChangeHandler } = useMouseContext();
+
+  const downIn = (delay: number) => {
+    return { y: 0, opacity: 1, transition: { duration: 0.3, delay: delay, ease: "easeOut" } };
+  };
+
+  const upOut = (delay: number) => {
+    return { y: 100, opacity: 0, transition: { duration: 0.2, delay: delay, ease: "easeOut" } };
+  };
+
   return (
     <s.Container>
       <s.Glow />
       <s.Border />
       <s.Background></s.Background>
       <s.ContentContainer>
-        <s.IconContainer>
-          <s.IconLink
+        <s.IconRow>
+          <s.IconContainer
             onMouseEnter={() => {
               cursorChangeHandler("mail");
             }}
             onMouseLeave={() => {
               cursorChangeHandler("");
             }}
+            initial={{ y: 200, opacity: 0 }}
+            animate={direction === "down" && origin === index - 1 ? downIn(0.6) : upOut(0)}
           >
             <CustomLink link="mailto:daoma90@gmail.com">
               <Icons icon="mail-circle" color="primaryAccent" size={50} />
             </CustomLink>
-          </s.IconLink>
-          <s.IconLink
+          </s.IconContainer>
+          <s.IconContainer
             onMouseEnter={() => {
               cursorChangeHandler("open");
             }}
             onMouseLeave={() => {
               cursorChangeHandler("");
             }}
+            animate={direction === "down" && origin === index - 1 ? downIn(0.7) : upOut(0.1)}
           >
             <CustomLink link="https://github.com/daoma90" target="_blank">
               <Icons icon="github" color="primaryAccent" size={50} />
             </CustomLink>
-          </s.IconLink>
-          <s.IconLink
+          </s.IconContainer>
+          <s.IconContainer
             onMouseEnter={() => {
               cursorChangeHandler("open");
             }}
             onMouseLeave={() => {
               cursorChangeHandler("");
             }}
+            animate={direction === "down" && origin === index - 1 ? downIn(0.8) : upOut(0.2)}
           >
             <CustomLink link="https://www.linkedin.com/in/davidoman90/" target="_blank">
               <Icons icon="linkedin" color="primaryAccent" size={50} />
             </CustomLink>
-          </s.IconLink>
-        </s.IconContainer>
+          </s.IconContainer>
+        </s.IconRow>
       </s.ContentContainer>
     </s.Container>
   );
--- components/library/organisms/GlowCards/index.tsx
+++ components/library/organisms/GlowCards/index.tsx
@@ -1,66 +1,151 @@
 import { utils } from "@/theme";
 import { useInView } from "framer-motion";
-import React, { useEffect, useRef, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import TextReveal from "../../atoms/TextReveal";
 import { BodyRegular, H2 } from "../../atoms/typography";
 import GlowCard from "../../molecules/GlowCard";
 import * as s from "./styles";
+import { m } from "framer-motion";
 
-const GlowCards = () => {
+interface GlowCardsProps {
+  direction: string;
+  index: number;
+  origin: number;
+}
+
+const GlowCards: FC<GlowCardsProps> = ({ direction, index, origin }) => {
   const [currentCard, setCurrentCard] = useState<number | null>(null);
   const ref = useRef(null);
-  const inView = useInView(ref, { amount: 0.5 });
+  const inView = useInView(ref, { amount: 0.1 });
 
   const handleSetCurrentCard = (index: number | null) => {
     setCurrentCard(index);
   };
 
+  const downIn = (delay: number) => {
+    return { y: 0, opacity: 1, transition: { duration: 0.3, delay: delay, ease: "easeOut" } };
+  };
+
+  const downOut = (delay: number) => {
+    return { y: -200, opacity: 0, transition: { duration: 0.3, delay: delay, ease: "easeOut" } };
+  };
+
+  const upIn = (delay: number) => {
+    return { y: 0, opacity: 1, transition: { duration: 0.3, delay: delay, ease: "easeOut" } };
+  };
+
+  const upOut = (delay: number) => {
+    return { y: 200, opacity: 0, transition: { duration: 0.3, delay: delay, ease: "easeOut" } };
+  };
   return (
     <s.Container ref={ref}>
-      <TextReveal inView={inView} direction="x" blockDelay={1} blockDuration={1}>
-        <H2>What can I do?</H2>
-      </TextReveal>
-      <s.CardContainer>
-        <GlowCard
-          inView={inView}
-          gradient="bluePurple"
-          header="Web"
-          items={[
-            { title: "Javascript", icon: "js", iconSize: 25 },
-            { title: "React", icon: "react", iconSize: 25 },
-            { title: "Next.js", icon: "next", iconSize: 25 },
-          ]}
-          index={0}
-          currentCard={currentCard}
-          handleSetCurrentCard={handleSetCurrentCard}
-        />
-        <GlowCard
-          inView={inView}
-          gradient="orangeRed"
-          header="Mobile"
-          items={[
-            { title: "React native", icon: "react", iconSize: 25 },
-            { title: "Expo", icon: "expo", iconSize: 25 },
-          ]}
-          index={1}
-          currentCard={currentCard}
-          handleSetCurrentCard={handleSetCurrentCard}
-        />
-        <GlowCard
-          inView={inView}
-          gradient="yellowGreen"
-          header="Design"
-          items={[{ title: "Figma", icon: "figma", iconSize: 25 }]}
-          index={2}
-          currentCard={currentCard}
-          handleSetCurrentCard={handleSetCurrentCard}
-        />
-      </s.CardContainer>
-      <s.Row>
-        <TextReveal inView={inView} direction="x" blockDelay={5} blockDuration={1}>
+      <m.div
+        initial={{ y: 0, opacity: 1 }}
+        animate={
+          direction === "down" && origin === index
+            ? downOut(0)
+            : direction === "down" && origin === index - 1
+            ? downIn(0.3)
+            : direction === "up" && origin === index + 1
+            ? upIn(0.3)
+            : direction === "up" && origin === index && upOut(0)
+        }
+      >
+        <TextReveal inView={inView} direction="x" blockDelay={0.7} blockDuration={1}>
+          <H2>What can I do?</H2>
+        </TextReveal>
+      </m.div>
+      <s.CardRow>
+        <s.CardContainer
+          initial={{ y: 0, opacity: 1 }}
+          animate={
+            direction === "down" && origin === index
+              ? downOut(0)
+              : direction === "down" && origin === index - 1
+              ? downIn(0.3)
+              : direction === "up" && origin === index + 1
+              ? upIn(0.3)
+              : direction === "up" && origin === index && upOut(0)
+          }
+        >
+          <GlowCard
+            inView={inView}
+            gradient="bluePurple"
+            header="Web"
+            items={[
+              { title: "Javascript", icon: "js", iconSize: 25 },
+              { title: "React", icon: "react", iconSize: 25 },
+              { title: "Next.js", icon: "next", iconSize: 25 },
+            ]}
+            index={0}
+            currentCard={currentCard}
+            handleSetCurrentCard={handleSetCurrentCard}
+          />
+        </s.CardContainer>
+        <s.CardContainer
+          initial={{ y: 0, opacity: 1 }}
+          animate={
+            direction === "down" && origin === index
+              ? downOut(0.1)
+              : direction === "down" && origin === index - 1
+              ? downIn(0.4)
+              : direction === "up" && origin === index + 1
+              ? upIn(0.4)
+              : direction === "up" && origin === index && upOut(0.1)
+          }
+        >
+          <GlowCard
+            inView={inView}
+            gradient="orangeRed"
+            header="Mobile"
+            items={[
+              { title: "React native", icon: "react", iconSize: 25 },
+              { title: "Expo", icon: "expo", iconSize: 25 },
+            ]}
+            index={1}
+            currentCard={currentCard}
+            handleSetCurrentCard={handleSetCurrentCard}
+          />
+        </s.CardContainer>
+        <s.CardContainer
+          initial={{ y: 0, opacity: 1 }}
+          animate={
+            direction === "down" && origin === index
+              ? downOut(0.2)
+              : direction === "down" && origin === index - 1
+              ? downIn(0.5)
+              : direction === "up" && origin === index + 1
+              ? upIn(0.5)
+              : direction === "up" && origin === index && upOut(0.2)
+          }
+        >
+          <GlowCard
+            inView={inView}
+            gradient="yellowGreen"
+            header="Design"
+            items={[{ title: "Figma", icon: "figma", iconSize: 25 }]}
+            index={2}
+            currentCard={currentCard}
+            handleSetCurrentCard={handleSetCurrentCard}
+          />
+        </s.CardContainer>
+      </s.CardRow>
+      <s.Row
+        initial={{ y: 0, opacity: 1 }}
+        animate={
+          direction === "down" && origin === index
+            ? downOut(0)
+            : direction === "down" && origin === index - 1
+            ? downIn(0.3)
+            : direction === "up" && origin === index + 1
+            ? upIn(0.3)
+            : direction === "up" && origin === index && upOut(0)
+        }
+      >
+        <TextReveal inView={inView} direction="x" blockDelay={4} blockDuration={1}>
           <BodyRegular>Is that it?</BodyRegular>
         </TextReveal>
-        <TextReveal inView={inView} direction="x" blockDelay={7} blockDuration={1}>
+        <TextReveal inView={inView} direction="x" blockDelay={6} blockDuration={1}>
           <BodyRegular>NO!</BodyRegular>
         </TextReveal>
       </s.Row>
