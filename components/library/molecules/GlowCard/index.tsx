dce5498d5064cc1eed08245a52501739126a7c90
David <Daoma90@gmail.com>
2023-05-28 17:01:07 +0200

    misc

--- components/library/molecules/GlowCard/index.tsx
+++ components/library/molecules/GlowCard/index.tsx
@@ -17,7 +17,7 @@ interface GlowCardProps {
 const duration = 0.5;
 const ease = [0.165, 0.84, 0.44, 1];
 const y = 100;
-const delay = 3;
+const delay = 2;
 
 const GlowCard: FC<GlowCardProps> = ({
   gradient,
